6d3fedd1cd029874a9a3c60c487bedecd2775620
akash-rawal <akashrawal444gmail.com>
2025-07-10 23:10:10 +0530

    authentication api created and added feture like regiteruser,loginuser,upload files,jwt verification

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use('/uploads', express.static('uploads'));
+
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+connectDB();
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/auth", authRoutes);
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
