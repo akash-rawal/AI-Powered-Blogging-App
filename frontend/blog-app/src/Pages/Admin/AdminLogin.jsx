f385aa486932c02deee3cf6681584de7c5061f22
akash-rawal <akashrawal444gmail.com>
2025-07-13 00:40:58 +0530

    login component created

--- frontend/blog-app/src/Pages/Admin/AdminLogin.jsx
+++ frontend/blog-app/src/Pages/Admin/AdminLogin.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
 
-import DashboardLayout from '../../components/Layouts/DashboardLayout'
+import LOGO from "../../assets/6534601.jpg";
+import Login from "../../components/Auth/Login";
+import SignUp from "../../components/Auth/SignUp";
 
 const AdminLogin = () => {
-  return (
-    <DashboardLayout>
-      admin login
-    </DashboardLayout>
-  )
-}
+    const [currentPage, setCurrentPage] = useState("login");
 
-export default AdminLogin
+    return (
+        <>
+            <div className="bg-white py-5 border-b border-gray-50">
+                <div className="container mx-auto">
+                    <img src={LOGO} alt="logo" className="h-[26px]" />
+                </div>
+            </div>
+
+            <div className="min-h-[calc(100vh-67px)] flex items-center justify-center">
+                <div className="bg-white rounded-2xl overflow-hidden shadow-2xl shadow-gray-200/60">
+                    {currentPage === "login"?(<Login setCurrentPage={setCurrentPage}/>):(<SignUp setCurrentPage={setCurrentPage} />)}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AdminLogin;
+
