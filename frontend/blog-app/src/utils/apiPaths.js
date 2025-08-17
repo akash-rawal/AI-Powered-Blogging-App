e7db79a7af1ed2f3f765e63dbee1b2eff49e7f8d
akash-rawal <akashrawal444gmail.com>
2025-08-17 13:39:38 +0530

    backend error fixed

--- frontend/blog-app/src/utils/apiPaths.js
+++ frontend/blog-app/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL =  "http://localhost:8000";
+export const BASE_URL =  "https://ai-powered-blogging-app.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
