bdc32133a975692aaad76b966a3fe36b1c5839ab
Akash rawal <158574393+akash-rawal@users.noreply.github.com>
2026-05-13 00:29:43 +0530

    Changed depricited model with new available model

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -16,7 +16,7 @@ const generateBlogPost = async (req, res) => {
      }
         const prompt = `Write a markdown-formatted blogPost titled "${title}". use a ${tone} tone. Include an introduction subheadings code examples if relevalent and a conclusion`;
      const response = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.5-flash-lite",
         contents:prompt,
      });
      let rawText = response.text;
@@ -40,7 +40,7 @@ if (!topics) {
 const prompt = blogPostIdeasPrompt(topics);
 
 const response = await ai.models.generateContent({
-  model: "gemini-2.0-flash-lite",
+  model: "gemini-2.5-flash-lite",
   contents: prompt,
 });
 
@@ -74,7 +74,7 @@ if (!content) {
 const prompt = generateReplyPrompt({ author, content });
 
 const response = await ai.models.generateContent({
-  model: "gemini-2.0-flash-lite",
+  model: "gemini-2.5-flash-lite",
   contents: prompt,
 });
 
@@ -99,7 +99,7 @@ if (!content) {
 const prompt = blogSummaryPrompt(content);
 
 const response = await ai.models.generateContent({
-  model: "gemini-2.0-flash-lite",
+  model: "gemini-2.5-flash-lite",
   contents: prompt,
 });
 
